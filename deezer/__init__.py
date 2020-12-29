013ee4fac3d0a24d8fe1d3636020bb0a590088b2
RemixDev <RemixDev64@gmail.com>
2020-12-29 21:17:58 +0100

    Version bump to 0.0.12

--- deezer/__init__.py
+++ deezer/__init__.py
@@ -4,7 +4,7 @@ from deezer.gw import GW
 from deezer.api import API
 import json
 
-__version__ = "0.0.11"
+__version__ = "0.0.12"
 
 class TrackFormats():
     """Number associtation for formats"""
