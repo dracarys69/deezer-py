dd0f5c6a64da39c17ff1349e3390028f412e37a9
RemixDev <RemixDev64@gmail.com>
2021-08-11 13:10:52 +0200

    v1.1.3

--- deezer/__init__.py
+++ deezer/__init__.py
@@ -4,7 +4,7 @@ from deezer.gw import GW
 from deezer.api import API
 from deezer.errors import DeezerError, WrongLicense, WrongGeolocation
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 
 class TrackFormats():
     """Number associtation for formats"""
